9b387602a5a478cc8eae9de6ec7ae633c3ac2b31
Learner <linhdqtg@gmail.com>
2019-07-02 04:05:34 +0700

    Revert default storage port
    It will cause more mistakes from users

--- src/cryptonote_core/cryptonote_core.cpp
+++ src/cryptonote_core/cryptonote_core.cpp
@@ -184,7 +184,7 @@ namespace cryptonote
   , "The port on which this service node's storage server is accessible. A listening "
     "storage server is required for service nodes. (This option is specified "
     "automatically when using BitToro Launcher.)"
-  , STORAGE_DEFAULT_PORT
+  , 0
   };
   static const command_line::arg_descriptor<std::string> arg_block_notify = {
     "block-notify"
